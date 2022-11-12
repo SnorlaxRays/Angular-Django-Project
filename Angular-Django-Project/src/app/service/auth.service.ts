078fc134691a69e8950f2a8bf2ccb2041e7baac8
SnorlaxRays <sandeepratnawat1106@gmail.com>
2022-11-12 14:00:47 +0530

    success

---
+++ Angular-Django-Project/src/app/service/auth.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+/**
+ * Provides REST CRUD operations of User functionality
+ * Each method of this class receives response callback method
+ * Response callback method is called by Observable and passed data and error
+ */
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  //Rest endPoint
+  endpoint = "http://localhost:8000/ORSAPI/Login/";
+  locatar:any;
+  form: any;
+
+  /**   Constructor injects Http service
+   * 
+   * @param http
+   */
+  
+  constructor(private http: HttpClient) { }
+  /**
+   * 
+   * @param form
+   * 
+   */
+  auth(form:any,compCB:any){
+    let url = this.endpoint + "auth";
+    this.http.post(url,form).subscribe(
+      (data) =>{
+        compCB(data);
+      },
+      (data) =>{
+        compCB(data,true);
+      }
+    );
+  }
+  
+}
